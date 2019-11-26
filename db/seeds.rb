1f32bb752e109a67bc8649dd74fc06c6d22bcbcf
Paul Carde <paul.carde@hotmail.fr>
2019-11-26 11:11:01 +0100

    start

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating Vignerons...'
+vignerons_attributes = [
+  {
+    first_name: 'Phillipe',
+    last_name: 'Zinck',
+    region:  'Alsace',
+    domain_name: 'Domaine Zinck'
+    description: "Le domaine familial est situé sur les coteaux à pente douce d'Eguisheim et de Gueberschwihr, à quelques kilomètres au sud de Colmar, dont les sols sont à dominante argilo-calcaire. Ils pratiquent une culture biologique non certifiée sur le vignoble."
+    avatar_photo: image_tag('/app/assets/images/Vignerons/avatar/zinck.png')
+    domain_photo: 'https://s.lesgrappes.com/media/osaka/default_cover.jpg'
+    cepage: 'Pinot Blanc, Riesling'
+    address: '18 Rue des 3 Châteaux'
+    city: 'Eguisheim'
+    zip_code: '68420'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    description:  'Pizzeria with industrial looks, serving rustic pizza and antipasti.',
+    stars:        4
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
