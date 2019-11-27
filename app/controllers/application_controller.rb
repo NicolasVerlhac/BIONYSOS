6319780a4ccaa9a1a7941240d061a1f4cf2ae895
Muriel Oger <muriel.oger@gmail.com>
2019-11-27 15:38:53 +0100

    devise parameter sanitizer ok dans application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,33 +7,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :color_red, :color_white, :color_rose, :region_alsace, :region_bordeaux, :region_rhone, :region_bourgogne, :region_loire, :classification_biodynamique, :classification_naturel, :alcool_light, :alcool_strong, :vin_sec, :vin_fruite, :vin_mineral, :vin_sucre, :vin_leger, :vin_puissant, :vin_tannique])
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
-
-
-
-    # add_column :users, :color_red, :boolean
-    # add_column :users, :color_white, :boolean
-    # add_column :users, :color_rose, :boolean
-    # add_column :users, :region_alsace, :boolean
-    # add_column :users, :region_bordeaux, :boolean
-    # add_column :users, :region_rhone, :boolean
-    # add_column :users, :region_bourgogne, :boolean
-    # add_column :users, :region_loire, :boolean
-    # add_column :users, :classification_biodynamique, :boolean
-    # add_column :users, :classification_naturel, :boolean
-    # add_column :users, :alcool_light, :boolean
-    # add_column :users, :alcool_strong, :boolean
-    # add_column :users, :vin_sec, :boolean
-    # add_column :users, :vin_fruite, :boolean
-    # add_column :users, :vin_mineral, :boolean
-    # add_column :users, :vin_sucre, :boolean
-    # add_column :users, :vin_leger, :boolean
-    # add_column :users, :vin_puissant, :boolean
-    # add_column :users, :vin_tannique, :boolean
-
 
 end
