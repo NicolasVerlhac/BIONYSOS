611366079a5dd477505ac8af231a07262241cd3e
Paul Carde <paul.carde@hotmail.fr>
2019-11-28 18:04:37 +0100

    ok

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,5 +3,11 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
 import { initMapbox } from '../components/mapbox';
-
 initMapbox();
+
+import { changeSignupTabOnClick } from '../components/change_signup_tab_on_click';
+changeSignupTabOnClick();
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
