aba2fe8a4db714659520768603f26232a9ebb6ce
Muriel Oger <muriel.oger@gmail.com>
2019-12-02 18:09:36 +0100

    correction bug pour accéder au dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,14 @@
 class DashboardController < ApplicationController
 
   def profile
-
-    @user = current_user
     @box = Box.find_by_id(params[:id])
-    # @wines = @box.wines
+    @user = current_user
+    @user.update(user_params)
+    @last_box = current_user.boxes.order(created_at: :asc).last
+    @last_box.wines
+
+
+
 
   end
 
@@ -14,5 +18,11 @@ class DashboardController < ApplicationController
     @last_box = current_user.boxes.order(created_at: :asc).last
     @last_box.wines
 
+  end
+
+
+  def user_params
+    params
+    .permit(:address, :zip_code, :city, :card_number, :expiry_date, :security_code)
   end
 end
