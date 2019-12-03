542e4353d1c98fe51775555d7146d25fdacfc839
Nicolas Verlhac <n.verlhac@mailistec.fr>
2019-12-03 17:33:37 +0100

    dashboard presque ok

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,10 +6,6 @@ class DashboardController < ApplicationController
     @user.update(user_params)
     @last_box = current_user.boxes.order(created_at: :asc).last
     @last_box.wines
-
-
-
-
   end
 
   def update
