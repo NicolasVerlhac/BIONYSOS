1aef9ad3399d100f6adcb057974b36c67a87870c
Paul Carde <paul.carde@hotmail.fr>
2019-12-03 16:57:16 +0100

    delete ok

--- app/controllers/box_items_controller.rb
+++ app/controllers/box_items_controller.rb
@@ -5,6 +5,12 @@ class BoxItemsController < ApplicationController
   end
 
   def create
+    @box_item = Wine.find(params[:wine_id])
+    if @box_item.nil?
+      @box_item = current_wine
+    @box_item.save
+    redirect_to boxes_pending_box_path
+    end
   end
 
   def destroy
